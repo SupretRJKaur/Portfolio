c9401ed763f01d3644028203e3153e7d45017348
Supret RJ Kaur <supreet01995@gmail.com>
2024-09-18 03:22:36 +0530

    updates

---
+++ script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.Intersecting) {
+            entry.target.classlist.add('show');
+        } else{
+            entry.target.classlist.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
